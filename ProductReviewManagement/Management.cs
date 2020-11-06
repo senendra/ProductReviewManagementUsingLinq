50ae3a49e055173fba79c5c8b79745cc6d91ceae
Senendra <senendra.007@gmail.com>
2020-11-07 02:07:50 +0530

    [Senendra] Add: Method to retrieve selected record using linq

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -20,5 +20,17 @@ namespace ProductReviewManagement
                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
             }
         }
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.ProducID == 1 || productReviews.ProducID == 4 || productReviews.ProducID == 9)
+                               && productReviews.Rating > 3
+                               select productReviews;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
     }
 }
